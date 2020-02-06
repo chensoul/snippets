232fa815948067fe7501f7bf0e5ee9fbc547a5a6
chenzj <junecloud@163.com>
2020-02-06 20:16:11 +0800

    up

--- dockerfiles/confluence/docker-entrypoint.sh
+++ dockerfiles/confluence/docker-entrypoint.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-# https://github.com/zhangguanzhang/Dockerfile/blob/master/atlassian-confluence/docker-entrypoint.sh
-
 set -e
 [[ "${DEBUG}" == "true" ]] && set -x
 
 # check if the `server.xml` file has been changed since the creation of this
 # Docker image. If the file has been changed the entrypoint script will not
 # perform modifications to the configuration file.
-if [ "$(stat -c "%Y" "/opt/atlassian/confluence/conf/server.xml")" -eq "0" ]; then
+if [ "$(stat -c "%Y" "${CONF_INSTALL}/conf/server.xml")" -eq "0" ]; then
   if [ -n "${X_PROXY_NAME}" ]; then
-    sed -i 's/proxyName=""/proxyName="${X_PROXY_NAME}"/g' /opt/atlassian/confluence/conf/server.xml
+    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8090"]' --type "attr" --name "proxyName" --value "${X_PROXY_NAME}" "${CONF_INSTALL}/conf/server.xml"
   fi
   if [ -n "${X_PROXY_PORT}" ]; then
-    sed -i 's/proxyName=""/proxyName="${X_PROXY_PORT}"/g' /opt/atlassian/confluence/conf/server.xml
+    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8090"]' --type "attr" --name "proxyPort" --value "${X_PROXY_PORT}" "${CONF_INSTALL}/conf/server.xml"
   fi
   if [ -n "${X_PROXY_SCHEME}" ]; then
-    sed -i 's/http/${X_PROXY_SCHEME}/g' /opt/atlassian/confluence/conf/server.xml
+    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8090"]' --type "attr" --name "scheme" --value "${X_PROXY_SCHEME}" "${CONF_INSTALL}/conf/server.xml"
   fi
   if [ -n "${X_PROXY_SECURE}" ]; then
-    sed -i 's/secure=""/secure="${X_PROXY_SECURE}"/g' /opt/atlassian/confluence/conf/server.xml
+    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8090"]' --type "attr" --name "secure" --value "${X_PROXY_SECURE}" "${CONF_INSTALL}/conf/server.xml"
+  fi
+  if [ -n "${X_PATH}" ]; then
+    xmlstarlet ed --inplace --pf --ps --update '//Context[@docBase="../confluence"]/@path' --value "${X_PATH}" "${CONF_INSTALL}/conf/server.xml"
   fi
 fi
 
