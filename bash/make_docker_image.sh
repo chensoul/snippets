51d5530f1cbf4e7503075d79f758beaeddd71a78
chenzj <junecloud@163.com>
2020-04-30 15:45:53 +0800

    update

--- bash/make_docker_image.sh
+++
@@ -1,9 +0,0 @@
-wget https://raw.githubusercontent.com/moby/moby/master/contrib/mkimage-yum.sh
-
-sudo sh mkimage-yum.sh centos
-#sudo sh mkimage-yum.sh centos -t 7
-
-
-wget https://raw.githubusercontent.com/moby/moby/master/contrib/mkimage-alpine.sh
-
-sudo sh mkimage-alpine.sh -r v3.11 -s
